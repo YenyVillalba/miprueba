d47d586a7515da68856768ae9f9e5c30184aecef
YenyVillalba <yeny_0192@hotmail.com>
2024-11-06 22:53:48 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // src/App.js
 import React from 'react';
 import Message from './Message';
+import cursoc from './Assets/curso-C++.jpg';
 
 function App() {
   return (
     <div>
       <h1>Bienvenido a mi Proyecto React yeny</h1>
+      <img src={cursoc} alt="Descripción de la imagen" />
       <Message />
     </div>
   );
