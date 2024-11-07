491e09a580c680d4df607dae0fbf0f22209bd049
YenyVillalba <yeny_0192@hotmail.com>
2024-11-06 22:46:22 -0500

    first commit

---
+++ src/Message.js
@@ -0,0 +1,11 @@
+
+// src/Message.js
+import React from 'react';
+
+function Message() {
+  return (
+    <p>Este es un mensaje de un componente de React simple.</p>
+  );
+}
+
+export default Message;
